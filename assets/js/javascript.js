773497881ca0205d7be88908350253651ee6c01c
NannaFredsgaard <nannafredsgaard@hotmail.com>
2020-11-22 23:48:14 +0100

    rigtigjstilføjet

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,11 +1,16 @@
 var i = 0;
-var txt = 'HELLO,'; 
-var speed = 100;
+
+var txt = 'HELLO,';
+
+var speed = 400;
 
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("typing").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+    if (i < txt.length) {
+      document.getElementById("typing").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
   }
-}
+
+  typeWriter();
+
