5957139513b06c2449d01d449a1518f4ffefd71a
NannaFredsgaard <nannafredsgaard@hotmail.com>
2020-11-22 22:58:40 +0100

    typingidandjsfile

---
+++ assets/js/javascript.js
@@ -0,0 +1,10 @@
+var txt = 'HELLO,'; 
+var speed = 100;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typing").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
